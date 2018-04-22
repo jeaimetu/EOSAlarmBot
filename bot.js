9d973a920a9bdffb65209ed4dc8c3338a9378fda
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-04-22 15:25:43 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,12 +3,51 @@ const config = require('./config'); // Configuration file that holds telegraf_to
 const session = require('telegraf/session')
 const Extra = require('telegraf/extra')
 const Markup = require('telegraf/markup')
+const Composer = require('telegraf/composer')
+const WizardScene = require('telegraf/scenes/wizard')
+const Stage = require('telegraf/stage')
+
+const stepHandler = new Composer()
+stepHandler.action('next', (ctx) => {
+  ctx.reply('Step 2. Via inline button')
+  return ctx.wizard.next()
+})
+stepHandler.command('next', (ctx) => {
+  ctx.reply('Step 2. Via command')
+  return ctx.wizard.next()
+})
+stepHandler.use((ctx) => ctx.replyWithMarkdown('Press `Next` button or type /next'))
+
+const superWizard = new WizardScene('super-wizard',
+  (ctx) => {
+    ctx.reply('Step 1', Markup.inlineKeyboard([
+      Markup.urlButton('❤️', 'http://telegraf.js.org'),
+      Markup.callbackButton('➡️ Next', 'next')
+    ]).extra())
+    return ctx.wizard.next()
+  },
+  stepHandler,
+  (ctx) => {
+    ctx.reply('Step 3')
+    return ctx.wizard.next()
+  },
+  (ctx) => {
+    ctx.reply('Step 4')
+    return ctx.wizard.next()
+  },
+  (ctx) => {
+    ctx.reply('Done')
+    return ctx.scene.leave()
+  }
+)
 
 const bot = new Telegraf(config.telegraf_token);    // Let's instantiate a bot using our token.
+const stage = new Stage([superWizard], { default: 'super-wizard' })
 
 // // Register session middleware
 bot.use(session())
 bot.use(Telegraf.log())
+bot.use(stage.middleware())
 
 const keyboard = Markup.inlineKeyboard([
   Markup.urlButton('카카오톡', 'http://telegraf.js.org'),
