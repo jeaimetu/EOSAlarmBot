81a8d28b571ec8281488a180b6eb3da9c1dca219
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-12 09:53:18 +0900

    Create partner.js

---
+++ partner.js
@@ -0,0 +1,9 @@
+module.exports.makePartnerMessage = function(){
+  
+  let msg = "토큰뱅크와의 파트너쉽으로 개발되었습니다. "; 
+  msg += "암호화폐 투자자와 검증된 ICO 를 잇는 아시아 최대의 ICO 투자 플랫폼입니다.";
+  msg += "\n" + "\n";
+  msg += "www.tokenbank.co.kr";
+
+  return msg;
+}
