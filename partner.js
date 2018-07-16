65735f9e74821414e79fcf0d4bbda339826ce7d6
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-16 14:29:32 +0900

    Update partner.js

--- partner.js
+++ partner.js
@@ -1,7 +1,8 @@
 module.exports.makePartnerMessage = function(){
   
   let msg = "토큰뱅크와의 파트너쉽으로 개발되었습니다. " + "\n\n" ;
-  msg += "토큰 뱅크는 자체 개발한 투표 툴로, EOS BP 투표를 지원하고," + "\n";
+  msg += "토큰 뱅크는 자체 개발한 투표 툴로, EOS BP 투표를 지원합니다." + "\n";
+  msg += "www.hexbp.com" "\n\n" ;
   msg += "암호화폐 투자자와 검증된 ICO 를 잇는 아시아 최대의 ICO 투자 플랫폼입니다.";
   msg += "\n" + "\n";
   msg += "www.tokenbank.co.kr";
