97e25096d9ff31b999d2747823eb7cc202fdec33
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-17 13:36:16 +0900

    Update partner.js

--- partner.js
+++ partner.js
@@ -1,11 +1,12 @@
 module.exports.makePartnerMessage = function(){
   
-  let msg = "토큰뱅크와의 파트너쉽으로 개발되었습니다. " + "\n\n" ;
-  msg += "토큰 뱅크는 자체 개발한 투표 툴로, EOS BP 투표를 지원합니다." + "\n";
-  msg += "www.hexbp.com" + "\n\n" ;
-  msg += "암호화폐 투자자와 검증된 ICO 를 잇는 아시아 최대의 ICO 투자 플랫폼입니다.";
-  msg += "\n" + "\n";
+  let msg = "헥슬란트는 현재 다양한 방면으로 EOS Cafe와 협업을 논의중입니다. " + "\n\n" ;
+  msg += "-토큰뱅크는 헥슬란트에서 서비스하는 아시아 최대 ICO 투자 플랫폼입니다." + "\n";
+  msg += "www.tokenbank.co.kr" + "\n\n" ;
+  msg += "-헥스비피는 헥슬란트에서 자체 개발한 투표툴로 다양한 BP공약확인 및 투표를 지원합니다.";
+  msg += "\n";
   msg += "www.tokenbank.co.kr";
+ 
 
   return msg;
 }
