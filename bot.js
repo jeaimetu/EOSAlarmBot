ce993b393357c07f27804ca50647f620811c3063
jeaimetu <32817067+jeaimetu@users.noreply.github.com>
2018-07-18 08:26:55 +0900

    Update bot.js

--- bot.js
+++ bot.js
@@ -116,7 +116,7 @@ async function getCetosBalance(account){
 async function getTokenBalance(account, cb){
  //let [addBalance, dacBalance, cetosBalance,cetBalance, ednaBalance, horusBalance,eoxBalance, evrBalance, esbBalance, atdBalance,
  let [addBalance, dacBalance, cetBalance, ednaBalance, horusBalance,eoxBalance, evrBalance, esbBalance, atdBalance,
-      octBalance, iqBalance, pglBalance, poorBalance, chlBalance] = 
+      octBalance, iqBalance, pglBalance, poorBalance, chlBalance, ridlBalance] = 
      await Promise.all([getTokenBalanceEach(account, "eosadddddddd"),
                         getTokenBalanceEach(account, "eosdactokens"),
                         //getTokenBalanceEach(account, "gyztomjugage"),
@@ -131,7 +131,9 @@ async function getTokenBalance(account, cb){
                         getTokenBalanceEach(account, "everipediaiq"),
                         getTokenBalanceEach(account, "prospectorsg"),
                         getTokenBalanceEach(account, "poormantoken"),
-                        getTokenBalanceEach(account, "challengedac")
+                        getTokenBalanceEach(account, "challengedac"),
+			getTokenBalanceEach(account, "ridlridlcoin")
+			
                        ]);
 //console.log(addBalance, dacBalance, cetosBalance);
  msg = "Current account : " + account;
@@ -231,6 +233,13 @@ if(eoxBalance != null){
  msg += t[1] + " : " + t[0];}
  else
   msg += "POOR : 0";
+ msg += "\n";
+
+if(ridlBalance != null){
+   t = ridlBalance.split(" ");
+ msg += t[1] + " : " + t[0];}
+ else
+  msg += "RIDL : 0";
  msg += "\n";
  
  /*
