341bebb449797e9b5833f98701de9021d97e5cc9
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-05 22:30:08 +0900

    Update partner.js

--- partner.js
+++ partner.js
@@ -1,4 +1,4 @@
-module.exports.makePartnerMessage = function(){
+module.exports.makePartnerMessage2 = function(){
   
   let msg = "헥슬란트는 현재 다양한 방면으로 EOS Cafe와 협업을 논의중입니다. " + "\n\n" ;
   msg += "-토큰뱅크는 헥슬란트에서 서비스하는 아시아 최대 ICO 투자 플랫폼입니다." + "\n";
@@ -6,6 +6,18 @@ module.exports.makePartnerMessage = function(){
   msg += "-헥스비피는 헥슬란트에서 자체 개발한 투표툴로 다양한 BP공약확인 및 투표를 지원합니다.";
   msg += "\n";
   msg += "www.hexbp.com";
+ 
+
+  return msg;
+}
+
+module.exports.makePartnerMessage = function(){
+  
+  let msg = "제1회 EOSIO CDD(Community x DAPP x DAC) Meetup 개최 " + "\n\n" ;
+  msg += "8월 18일(토) 오후 5시, 마루180(강남구 역삼로 180)" + "\n";
+  msg += "https://goo.gl/G1wUbA" + "\n\n" ;
+  msg += "많은 참석 부탁 드립니다! 자세한 사항은 링크를 확인해 주세요.";
+  msg += "\n";
  
 
   return msg;
