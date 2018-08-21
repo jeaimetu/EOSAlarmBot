325706a16fd9c1e28fc0ca10304eb5f87e746f8d
Teddy <32817067+jeaimetu@users.noreply.github.com>
2018-08-21 09:01:49 +0900

    Update partner.js

--- partner.js
+++ partner.js
@@ -1,4 +1,4 @@
-module.exports.makePartnerMessage2 = function(){
+module.exports.makePartnerMessage3 = function(){
   
   let msg = "헥슬란트는 현재 다양한 방면으로 EOS Cafe와 협업을 논의중입니다. " + "\n\n" ;
   msg += "-토큰뱅크는 헥슬란트에서 서비스하는 아시아 최대 ICO 투자 플랫폼입니다." + "\n";
@@ -11,12 +11,23 @@ module.exports.makePartnerMessage2 = function(){
   return msg;
 }
 
-module.exports.makePartnerMessage = function(){
+module.exports.makePartnerMessage2 = function(){
   
   let msg = "제1회 EOSIO CDD(Community x DAPP x DAC) Meetup 개최 " + "\n\n" ;
   msg += "8월 18일(토) 오후 5시, 마루180(강남구 역삼로 180)" + "\n";
   msg += "https://goo.gl/ZsXFAA" + "\n\n" ;
   msg += "많은 참석 부탁 드립니다! 자세한 사항은 링크를 확인해 주세요.";
+  msg += "\n";
+ 
+
+  return msg;
+}
+
+module.exports.makePartnerMessage = function(){
+  
+  let msg = "DEXEOS dexeos.io " + "\n\n" ;
+  msg += "세계 최초 EOS기반 탈중앙화 거래소" + "\n";
+  msg += "dexeos.io 에서 에드랍 토큰을 거래하세요." + "\n\n" ;
   msg += "\n";
  
 
